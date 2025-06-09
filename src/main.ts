066de29031b008499c86078432f1662255b27fb0
Mateor13 <150361816+Mateor13@users.noreply.github.com>
2025-06-09 18:53:00 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,19 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from './environments/environment';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import { routes } from './app/app.routes';
+import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+
+
+defineCustomElements(window);
 
 bootstrapApplication(AppComponent, {
-  providers:[
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
     importProvidersFrom(IonicModule.forRoot()),
     appRouting,
     provideHttpClient(),
